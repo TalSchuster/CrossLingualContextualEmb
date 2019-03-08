514c4e85a8e6c869f0974ab9b6cbccda3156073b
TalSchuster <tals@csail.mit.edu>
2019-03-08 17:49:33 -0500

    get all models script

---
+++ get_models.sh
@@ -0,0 +1,9 @@
+mkdir models
+cd models
+wget https://www.dropbox.com/s/1h62kc1qdcuyy2u/en_weights.hdf5
+wget https://www.dropbox.com/s/ygfjm7zmufl5gu2/es_weights.hdf5
+wget https://www.dropbox.com/s/mm64goxb8wbawhj/fr_weights.hdf5
+wget https://www.dropbox.com/s/owfou7coi04dyxf/it_weights.hdf5
+wget https://www.dropbox.com/s/ul82jsal1khfw5b/pt_weights.hdf5
+wget https://www.dropbox.com/s/boptz21zrs4h3nw/sv_weights.hdf5
+wget https://www.dropbox.com/s/2kbjnvb12htgqk8/de_weights.hdf5
